894448443b93f76c90635570164fabf78eea96be
Axi <axelandre91@gmail.com>
2023-01-22 12:15:28 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  )
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockatiel",
+  //   }),
+  //   React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  // ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
